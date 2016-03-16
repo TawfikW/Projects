121e5d0ef180e6cf730c5d7073048a67b6772cf0
TawfikW <wasswass70@gmail.com>
2016-03-16 14:01:26 -0700

    Added AgeInDays drill
    Displays number of years in days, minutes & seconds

---
+++ Phython/AgeInDays.py
@@ -0,0 +1,8 @@
+print ("Let's see how long you have lived in days, minutes and seconds.")
+name = input("Name: ")
+print ("Now enter your age")
+age = int(input("Age: "))
+days = age * 365
+minutes = age * 525948
+seconds = age * 31556926
+print (name, "has been alive for", days,"days", minutes, "minutes and", seconds, "seconds! Wow!")
